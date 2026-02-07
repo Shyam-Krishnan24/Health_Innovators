0b9a4c46f32ce3c0964b59e53f624eb583a40d25
SHYAAM KRISHNAN R <168326697+Shyam-Krishnan24@users.noreply.github.com>
2026-02-07 14:50:45 +0530

    Delete Front-End (IVR system) directory

--- Front-End (IVR system)/languages.py
+++
@@ -1,8 +0,0 @@
-LANGUAGES = {
-    "1": {"name": "English", "welcome": "Welcome to Smart Health IVR"},
-    "2": {"name": "Tamil", "welcome": "ஸ்மார்ட் ஹெல்த் IVR-க்கு வரவேற்கிறோம்"},
-    "3": {"name": "Telugu", "welcome": "స్మార్ట్ హెల్త్ IVR కు స్వాగతం"},
-    "4": {"name": "Malayalam", "welcome": "സ്മാർട്ട് ഹെൽത്ത് IVR ലേക്ക് സ്വാഗതം"},
-    "5": {"name": "Hindi", "welcome": "स्मार्ट हेल्थ IVR में आपका स्वागत है"},
-    "6": {"name": "Kannada", "welcome": "ಸ್ಮಾರ್ಟ್ ಹೆಲ್ತ್ IVR ಗೆ ಸ್ವಾಗತ"}
-}
